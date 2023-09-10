53564f0462738e0e9df8ac190b223c9ef25dda80
Mete Sayan <metesayan99@gmail.com>
2023-09-10 21:49:33 +0300

    Create 83-remove-duplicates-from-sorted-list.ts

---
+++ LeetCode/83-remove-duplicates-from-sorted-list.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+import { ListNode } from './utils/ListNode';
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+	let now = head;
+	while (now) {
+		if (now.next && now.next.val === now.val) {
+			now.next = now.next.next;
+		} else {
+			now = now.next;
+		}
+	}
+	return head;
+}
