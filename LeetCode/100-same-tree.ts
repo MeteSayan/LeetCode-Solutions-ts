cbc90bd774210db7610ff2c6396c2a9f3f0669d8
Mete Sayan <metesayan99@gmail.com>
2023-09-20 13:21:00 +0300

    Create 100-same-tree.ts

---
+++ LeetCode/100-same-tree.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+import { TreeNode } from './utils/TreeNode';
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+	if (!p && !q) return true;
+	if (!p || !q) return false;
+
+	return (
+		p?.val === q?.val &&
+		isSameTree(p?.left, q?.left) &&
+		isSameTree(p?.right, q.right)
+	);
+}
