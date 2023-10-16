0583f6a0ecc37f800b59222a9d2bf1a4b15a603c
Mete Sayan <metesayan99@gmail.com>
2023-10-16 21:22:39 +0300

    [HackerRank] Java Stdin and Stdout I

---
+++ Java/HackerRank/Java/Introduction/Java Stdin and Stdout I/Solution.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int firstInt = scanner.nextInt();
+        int secondInt = scanner.nextInt();
+        int thirdInt = scanner.nextInt();
+        scanner.close();
+
+        System.out.println(firstInt);
+        System.out.println(secondInt);
+        System.out.println(thirdInt);
+    }
+}
--- Java/HackerRank/Java/Introduction/Welcome to Java!/Solution.java
+++ Java/HackerRank/Java/Introduction/Welcome to Java!/Solution.java
@@ -4,14 +4,7 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int firstInt = scanner.nextInt();
-        int secondInt = scanner.nextInt();
-        int thirdInt = scanner.nextInt();
-        scanner.close();
-
-        System.out.println(firstInt);
-        System.out.println(secondInt);
-        System.out.println(thirdInt);
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
     }
 }
