8d9388a8019ed00324629b55e203c7b28413111f
Mete Sayan <metesayan99@gmail.com>
2023-10-16 21:06:11 +0300

    [HackerRank] Day 00 - Hello, World! with Java

---
+++ Java/HackerRank/30 Days of Code/Day 00 - Hello, World/Solution.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String userInput = scn.nextLine();
+		System.out.println("Hello, World.");
+		System.out.println(userInput);
+		scn.close();
+	}
+}
