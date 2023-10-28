518ebbd46bdbbc3c2928ac4667ccaeae631ab942
Mete Sayan <metesayan99@gmail.com>
2023-10-28 23:24:29 +0300

    Create 0258-add-digits.ts

---
+++ TypeScript/Solutions/0258-add-digits.ts
@@ -0,0 +1,17 @@
+function addDigits(num: number): number {
+	let newNumber = 999;
+    let digits = num.toString().split('');
+	while (newNumber >= 10) {
+		newNumber = sumNums(digits);
+        if(newNumber >= 10) digits = newNumber.toString().split('');
+	}
+	return newNumber;
+}
+
+function sumNums(digits: string[]) {
+	let numSum = 0;
+	for (let x = 0; x < digits.length; x++) {
+		numSum += parseInt(digits[x]);
+	}
+	return numSum;
+}
