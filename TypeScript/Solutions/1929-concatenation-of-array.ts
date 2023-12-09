9b59bf4de155af3f569cef8fff53729cbdfa8e25
Mete Sayan <metesayan99@gmail.com>
2023-12-09 22:58:53 +0300

    Update 1929-concatenation-of-array.ts

--- TypeScript/Solutions/1929-concatenation-of-array.ts
+++ TypeScript/Solutions/1929-concatenation-of-array.ts
@@ -2,3 +2,15 @@ function getConcatenation(nums: number[]): number[] {
 	nums.push(...nums);
 	return nums;
 }
+
+// Another solution
+// function getConcatenation(nums: number[]): number[] {
+//     const len = nums.length;
+//     let ans =   nums;
+
+//     for (let i = 0; i < len; i++) {
+//         ans[i + len] = nums[i];
+//     }
+
+//     return ans;
+// };
