5f634f9a5bfdca2a84182bafc6a4703659288c9e
Mete Sayan <metesayan99@gmail.com>
2023-12-20 17:35:56 +0300

    Update 2000-reverse-prefix-of-word.ts

--- TypeScript/Solutions/2000-reverse-prefix-of-word.ts
+++ TypeScript/Solutions/2000-reverse-prefix-of-word.ts
@@ -12,3 +12,31 @@ function reversePrefix(word: string, ch: string): string {
 			.join('') + word.substring(indexOfChar + 1)
 	);
 }
+
+//! Solution 2
+// function reversePrefix(word: string, ch: string): string {
+// 	if (word.length === 1) return word;
+
+// 	let result: string = '';
+// 	const indexOfChar = word.indexOf(ch);
+
+// 	if (indexOfChar === 0) return word;
+// 	if (indexOfChar) {
+// 		const str = [
+// 			word.slice(0, indexOfChar + 1),
+// 			word.slice(indexOfChar + 1, word.length),
+// 		];
+// 		const reversedStr = reverseStr(str[0]);
+// 		result = reversedStr + str[1];
+// 	}
+// 	return result;
+// }
+
+// function reverseStr(str: string) {
+// 	let reverseString: string = '';
+
+// 	for (let char of str) {
+// 		reverseString = char + reverseString;
+// 	}
+// 	return reverseString;
+// }
