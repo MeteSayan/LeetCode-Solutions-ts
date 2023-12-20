c72e7784c3366d7352f6c95f7974a0afac611aa9
Mete Sayan <metesayan99@gmail.com>
2023-12-20 16:58:46 +0300

    Create 2744-find-maximum-number-of-string-pairs.ts

---
+++ TypeScript/Solutions/2744-find-maximum-number-of-string-pairs.ts
@@ -0,0 +1,25 @@
+function maximumNumberOfStringPairs(words: string[]): number {
+	let count: number = 0;
+
+	for (let x = 0; x < words.length; x++) {
+		const reversedStr = reverseStr(words[x]);
+		for (let y = x + 1; y < words.length; y++) {
+			if (x == y) continue;
+			if (words[y] === reversedStr) {
+				count++;
+				break;
+			}
+		}
+	}
+
+	return count;
+}
+
+function reverseStr(str: string) {
+	let reverseString: string = '';
+
+	for (let char of str) {
+		reverseString = char + reverseString;
+	}
+	return reverseString;
+}
