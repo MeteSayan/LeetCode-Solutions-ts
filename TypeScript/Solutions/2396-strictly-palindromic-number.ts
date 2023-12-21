037eac83da32ee8280a80400929da8197eae7b77
Mete Sayan <metesayan99@gmail.com>
2023-12-21 23:14:27 +0300

    Create 2396-strictly-palindromic-number.ts

---
+++ TypeScript/Solutions/2396-strictly-palindromic-number.ts
@@ -0,0 +1,18 @@
+//! Worst question ever. I hate it. Not even worth the time to understand it.
+
+function isStrictlyPalindromic(n: number): boolean {
+	return false;
+}
+
+//  Solution 2 - Not mine
+//  function isStrictlyPalindromic(n: number): boolean {
+// 	let i = 2;
+// 	while (i <= n - 2) {
+// 		let conversion = n.toString(i);
+// 		if (conversion.split('').reverse().join('') != conversion) {
+// 			return false;
+// 		}
+// 		i++;
+// 	}
+// 	return true;
+// }
