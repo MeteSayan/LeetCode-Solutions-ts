5f56ac87412d5d9a5d49759a09e883f008c82d19
Mete Sayan <metesayan99@gmail.com>
2024-08-25 23:32:34 +0300

    Create 0145-binary-tree-postorder-traversal.ts

---
+++ Solutions/0145-binary-tree-postorder-traversal.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+import { TreeNode } from '../utils/TreeNode';
+
+function postorderTraversal(root: TreeNode | null): number[] {
+	return !root
+		? []
+		: [
+				...postorderTraversal(root.left),
+				...postorderTraversal(root.right),
+				root.val,
+		  ];
+}
